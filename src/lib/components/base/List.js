a6e7100a5f86ffacfaaf3caf5ebb8c069be4676b
Gabin Rimbault <rimbault.gabin@icloud.com>
2026-03-18 17:34:09 +0100

    fix: use anchor links for external URLs in List

--- src/lib/components/base/List.js
+++ src/lib/components/base/List.js
@@ -12,19 +12,26 @@ export default function List({ type = "ul", data = [], classList, classLink }) {
   } else if (typeof data === "object" && data !== null) {
     listItems = Object.entries(data).map(([key, value], index) => {
       const isExternalLink = value.liens[0].startsWith("http");
-      const linkProps = isExternalLink
-        ? { to: value.liens[0], target: "_blank", rel: "noopener noreferrer" }
-        : { to: value.liens[0] };
 
       return (
         <li key={index} id={key}>
-          <Link {...linkProps} className={classLink} alt={"Lien " + key}>
-            {value.icons && value.icons[0] === "left" && value.icons[1]}
-            {value.icons && value.icons[0] === "left" && " "}
-            {value.liens[1]}
-            {value.icons && value.icons[0] === "right" && " "}
-            {value.icons && value.icons[0] === "right" && value.icons[1]}
-          </Link>
+          {isExternalLink ? (
+            <a href={value.liens[0]} className={classLink}>
+              {value.icons && value.icons[0] === "left" && value.icons[1]}
+              {value.icons && value.icons[0] === "left" && " "}
+              {value.liens[1]}
+              {value.icons && value.icons[0] === "right" && " "}
+              {value.icons && value.icons[0] === "right" && value.icons[1]}
+            </a>
+          ) : (
+            <Link to={value.liens[0]} className={classLink}>
+              {value.icons && value.icons[0] === "left" && value.icons[1]}
+              {value.icons && value.icons[0] === "left" && " "}
+              {value.liens[1]}
+              {value.icons && value.icons[0] === "right" && " "}
+              {value.icons && value.icons[0] === "right" && value.icons[1]}
+            </Link>
+          )}
         </li>
       );
     });
