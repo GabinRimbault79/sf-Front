ccb32b7f31968d7382d963056d6e93e754e8d10b
Gabin Rimbault <rimbault.gabin@icloud.com>
2026-03-19 18:34:22 +0100

    chore: add eslint script and clean lint warnings

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import React from "react";
 import "./lib/scss/all.css";
-import {Content} from "./lib";
 
 function App() {
     return (
         <>
-            <Content>SimpleFramework FrontEnd</Content>
         </>
     )
 }
